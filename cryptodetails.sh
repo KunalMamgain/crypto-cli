ed423d73d104a1e5037cc5c4fa543e2e949e8182
KunalMamgain <mamgainkunal@gmail.com>
2021-06-04 22:55:00 +0530

    script added.

---
+++ cryptodetails.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl -sS -X GET "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=15&page=1&sparkline=false" -H "accept: application/json"  | jq -r '(["S.No.","Name","Current Price ($)","ATH ($)","Change (24H)","Market Cap ($)"] | (., map(length*"-"))), (.[] | [  ( .market_cap_rank|tostring),.symbol,"$ "+(.current_price|tostring),"$ "+(.ath|tostring),(.price_change_percentage_24h|tostring)+"%","$ "+(.market_cap|tostring)]) | @tsv'| sed 's/	/,|,/g' | column -s ',' -t | sed "s/\( -[0-9]*\.[0-9]*%\)/`tput setaf 9`\1`tput sgr 0`/g" | sed " s/\( [0-9]*\.[0-9]*%\)/`tput setaf 10`\1`tput sgr 0`/g" | sed "1s/\(.*\)/`tput setaf 11`\1`tput sgr0`/g"
+
